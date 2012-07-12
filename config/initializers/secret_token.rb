21b390fee6770cc39be575cc3dbc121cede7dc13
Adam Krawiec <bbttxu@gmail.com>
2012-07-11 20:46:44 -0500

    firs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Denton::Application.config.secret_token = 'bad4cc11ade78cf16449fe952a2b0a0f6c16fceea145d6fa9ccafe6fef9f7b18242c6abb6f2bdd32866fb0b09976e8dc74d85ef7841256dd981297b303f90213'
