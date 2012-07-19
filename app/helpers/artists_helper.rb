c9ed5e43386d4495a0baad1761595d4122b39f1a
Adam Krawiec <bbttxu@gmail.com>
2012-07-18 22:54:06 -0500

    it's safe to say this is better than it was

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,9 +1,27 @@
 module ArtistsHelper
   def split_on_important_stuff(stuff)
-    split_on_and split_on_semicolon stuff
+    split_of_the_life_of split_on_and split_on_semicolon stuff
     
   end
   
+  def split_of_the_life_of( names )
+    new_chunks = []
+    
+    names.each do |name|
+      if name.match("Of The Life Of") 
+        crap = []
+        crap << name.split("Of The Life Of").each_with_index do |chunk,i|
+          new_chunks[i] = "#{chunk} Of The Life Of".strip if i == 0
+          new_chunks[i] = "#{chunk}".strip unless i == 0
+        end
+      else
+        new_chunks << name
+      end
+    end
+    new_chunks.flatten
+  end
+
+
   def split_on_semicolon( names )
     new_chunks = []
     
