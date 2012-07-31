df9f9a3858c49c484d7460bfec9c0c549990849d
Adam Krawiec <bbttxu@gmail.com>
2012-07-31 00:03:26 -0500

    playing more with mobile grids

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -2,9 +2,7 @@ class ShowsController < ApplicationController
   # GET /shows
   # GET /shows.json
   def index
-    @shows = Show.all.group_by{ |u| Time.at(u.starts_at).to_date.to_datetime.to_i }
-
-
+    @shows = Show.upcoming.group_by{ |u| Time.at(u.starts_at).to_date.to_datetime.to_i }
 
     respond_to do |format|
       format.html # index.html.erb
