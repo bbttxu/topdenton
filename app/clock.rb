6bf00a66ccaf48d7acf8bc47fcd23ad6005a4b5d
Adam Krawiec <bbttxu@gmail.com>
2012-07-30 20:30:51 -0500

    taking out banter

--- app/clock.rb
+++ app/clock.rb
@@ -5,18 +5,16 @@ $stdout.sync = true
 require File.expand_path('../workers/dans.rb',  __FILE__)
 require File.expand_path('../workers/haileys.rb',  __FILE__)
 require File.expand_path('../workers/rubbergloves.rb',  __FILE__)
-# require File.expand_path('../workers/banter.rb',  __FILE__)
 require File.expand_path('../workers/andys.rb',  __FILE__)
 
 require 'clockwork'
 include Clockwork 
 require 'resque'
 
-every( 1.minutes, 'update.venues') { 
+every( 15.minutes, 'update.venues') { 
 	puts 'update venues'
   Resque.enqueue(Dans)
   Resque.enqueue(Haileys)
   Resque.enqueue(Rgrs)
-  # Resque.enqueue(Banter)
   Resque.enqueue(Andys)
 }
