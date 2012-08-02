242c738d6ca7775651c3641ae1bd5e70e996f199
Adam Krawiec <bbttxu@gmail.com>
2012-08-02 18:42:05 -0500

    fixing the showing of dates in the past

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,6 +5,7 @@ class Show < ActiveRecord::Base
   has_many :artists, { :through => :gigs, :order => "position DESC" }
   
   default_scope order("starts_at")
-  scope :upcoming, lambda { where("starts_at < ?", Time.zone.now.to_i + 7 * 24 * 60 * 60) } 
+  scope :upcoming, lambda { where("starts_at > ?", Time.zone.now.to_i - 6 * 60 * 60 ) } 
+  scope :next_week, lambda { where("starts_at < ?", Time.zone.now.to_i + 7 * 24 * 60 * 60) } 
   scope :by_day, lambda { group_by{ |u| Time.at(u.starts_at).to_date } }
 end
