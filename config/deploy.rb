c6e8a0fd8b3e3e94c92bede1f16e8bea3b1715b0
Adam Krawiec <bbttxu@gmail.com>
2012-08-19 22:38:16 -0500

    taking out broken cap tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -118,39 +118,39 @@ namespace :deploy do
     end
   end
 
-  desc "Zero-downtime restart of Unicorn"
-  task :restart, :except => { :no_release => true } do
-    run "kill -s USR2 `cat /tmp/unicorn.my_site.pid`"
-  end
-
-  desc "Start unicorn"
-  task :start, :except => { :no_release => true } do
-    run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
-  end
-
-  desc "Stop unicorn"
-  task :stop, :except => { :no_release => true } do
-    run "kill -s QUIT `cat /tmp/unicorn.my_site.pid`"
-  end
-
-  namespace :rollback do
-    desc "Moves the repo back to the previous version of HEAD"
-    task :repo, :except => { :no_release => true } do
-      set :branch, "HEAD@{1}"
-      deploy.default
-    end
-
-    desc "Rewrite reflog so HEAD@{1} will continue to point to at the next previous release."
-    task :cleanup, :except => { :no_release => true } do
-      run "cd #{current_path}; git reflog delete --rewrite HEAD@{1}; git reflog delete --rewrite HEAD@{1}"
-    end
-
-    desc "Rolls back to the previously deployed version."
-    task :default do
-      rollback.repo
-      rollback.cleanup
-    end
-  end
+  # desc "Zero-downtime restart of Unicorn"
+  # task :restart, :except => { :no_release => true } do
+  #   run "kill -s USR2 `cat /tmp/unicorn.my_site.pid`"
+  # end
+  # 
+  # desc "Start unicorn"
+  # task :start, :except => { :no_release => true } do
+  #   run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
+  # end
+  # 
+  # desc "Stop unicorn"
+  # task :stop, :except => { :no_release => true } do
+  #   run "kill -s QUIT `cat /tmp/unicorn.my_site.pid`"
+  # end
+  # 
+  # namespace :rollback do
+  #   desc "Moves the repo back to the previous version of HEAD"
+  #   task :repo, :except => { :no_release => true } do
+  #     set :branch, "HEAD@{1}"
+  #     deploy.default
+  #   end
+  # 
+  #   desc "Rewrite reflog so HEAD@{1} will continue to point to at the next previous release."
+  #   task :cleanup, :except => { :no_release => true } do
+  #     run "cd #{current_path}; git reflog delete --rewrite HEAD@{1}; git reflog delete --rewrite HEAD@{1}"
+  #   end
+  # 
+  #   desc "Rolls back to the previously deployed version."
+  #   task :default do
+  #     rollback.repo
+  #     rollback.cleanup
+  #   end
+  # end
 end
 
 # Foreman tasks
