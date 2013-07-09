f41980d6c985d49483aaf71ffcd21c516a1437c8
Adam Krawiec <adamkrawiec@gmail.com>
2013-07-08 22:12:06 -0500

    doing the damned thing

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -7,15 +7,17 @@ class ShowsController < ApplicationController
     # @shows.group_by{ |u| Time.zone.at(u.starts_at).to_date.to_datetime.to_i }
     @shows = Show.ordered.group_by{|x|Time.zone.parse("#{x.starts_at.to_date} 2:00am")}
     @dates = @shows.collect{|k,v|k}.sort
-    @first_show = Show.upcoming.ordered.first
-    @last_show = Show.upcoming.ordered.last
+    @next_show = Show.upcoming.ordered.first
+    @prev_show = Show.upcoming.ordered.last
 
-    @calendar = (@first_show.starts_at.beginning_of_week.to_date..@last_show.starts_at.end_of_month.to_date)
+    @calendar = (@next_show.starts_at.beginning_of_week.to_date..@prev_show.starts_at.end_of_month.to_date)
     @calendar = @calendar.each_with_object({}) do |num, hash|
       d = @shows[Time.zone.parse("#{num} 2:00am")]
       hash[num] = 0
       hash[num] = d.count unless d.nil?
     end
+
+
 
     respond_to do |format|
       format.html # index.html.erb
@@ -34,6 +36,8 @@ class ShowsController < ApplicationController
 
     redirect_to '/shows/today', :status => 302 and return if params[:date] == nil
 
+    @next_show = Show.after(Time.zone.parse( "#{params[:date]} 2:00am" ) + 24.hours).ordered.first
+    @prev_show = Show.before(Time.zone.parse( "#{params[:date]} 2:00am" )).ordered.last
 
     now = Time.zone.parse( "#{params[:date]} 2:00am" )
     tomorrow = now + 24 * 60 * 60
