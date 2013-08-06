3e7e25d1753c909b0804acbe911a7d5683823e59
Adam Krawiec <adamkrawiec@gmail.com>
2013-08-05 19:59:16 -0500

    top 5

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -8,6 +8,8 @@ class Rating
 
   before_save :downcase_tags
 
+  scope :top_five, ->(limit=5) { order_by([:rating, :asc]).limit(limit) }
+
   def downcase_tags
     self.tags = self.tags.downcase
   end
