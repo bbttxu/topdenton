270c5b6e1328e2c42ff534e5dd91724a10edac52
Adam Krawiec <adamkrawiec@gmail.com>
2014-05-18 13:35:24 -0500

    closer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to request.referer, :notice => "Signed out!"
+    redirect_to root_path, :notice => "Signed out!"
   end
 end
