03053d5461eaf98f8a745bcdf3dcfb99cb744054
Adam Krawiec <adamkrawiec@gmail.com>
2014-07-13 12:37:31 -0500

    updating user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,21 @@
 
 require 'spec_helper'
 
-describe "user" do
+describe User do
+
+  before(:each) do
+    @user = FactoryGirl.create :user
+  end
+
+  describe "model" do
+    it "is valid" do
+      @user.should be_valid()
+    end
+  end
 
   describe "roles" do
-
     it "assigns a default rater role" do
-    	user = FactoryGirl.create :user
-      expect(user.roles.count).to eq(1)
+      expect(@user.roles.count).to eq(1)
     end
   end
 end
